3fd7aeac32b984e7b61a82caf03bcd7bed37c79d
kalmydas-cloud <kalmydas@gmail.com>
2026-03-04 02:19:25 +0100

    Liquidite 3 modes: KAL+USDC, USDC seul, KAL seul

--- _next/static/chunks/webpack.js
+++ _next/static/chunks/webpack.js
@@ -197,7 +197,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "05324104a78b09e0"; }
+/******/ 		__webpack_require__.h = function() { return "69b5545d90a761c6"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
